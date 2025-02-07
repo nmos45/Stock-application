00d0def2db8e46100028cde5c8068a22daf5f2e2
Naoise Baoise <naoisebaoise@Naoises-MacBook-Air.local>
2025-02-07 19:23:26 +0000

    feat(forms):StockFood detail,update and delete form

--- StockTaker/inventory/views.py
+++ StockTaker/inventory/views.py
@@ -32,3 +32,41 @@ class StockInstanceDelete(LoginRequiredMixin, DeleteView):
     model=StockInstance
     success_url = reverse_lazy('homepage')   
 
+
+class StockFoodDetail(LoginRequiredMixin, generic.DetailView):
+    """class-based detail view to display specific StockFood instance"""
+    model=StockFood
+
+class StockFoodCreate(LoginRequiredMixin, CreateView):
+    """generic-edit view to create a stockood instance using forms"""
+    model=StockFood 
+    fields=['stock_instance','food','quantity','expiry_date']
+
+    def get_initial(self):
+        """Set initial field values in form"""
+        initial = super().get_initial()
+        stock_instance_id = self.kwargs.get('pk')
+        if stock_instance_id:
+            initial['stock_instance'] = stock_instance_id
+        return initial
+
+    def get_form(self, form_class=None):
+        """Customize the form to disable the stock_instance field (optional)"""
+        form = super().get_form(form_class)
+        if 'stock_instance' in form.fields:
+            form.fields['stock_instance'].widget.attrs['readonly'] = True  # Make it readonly
+        return form
+
+class StockFoodUpdate(LoginRequiredMixin, UpdateView):
+    """generic-edit view to update a stockood instance using forms"""
+    model=StockFood 
+    fields=['stock_instance','food','quantity','expiry_date']
+
+class StockFoodDelete(LoginRequiredMixin, DeleteView):
+    """generic-edit view to delete a StockFood using forms"""
+    model=StockFood
+    success_url = reverse_lazy('')
+
+    def get_success_url(self):
+        stockinstance = self.object.stock_instance 
+        return reverse('stockInstance-detail', kwargs={'pk':stockinstance.id})
