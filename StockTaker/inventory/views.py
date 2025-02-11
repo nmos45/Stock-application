f10294b17204d973e88f62c91b2b1a291b3f7dd0
Naoise Baoise <naoisebaoise@Naoises-MacBook-Air.local>
2025-02-11 17:46:22 +0000

    update(models):User field and verified field added to Food object

--- StockTaker/inventory/views.py
+++ StockTaker/inventory/views.py
@@ -5,6 +5,7 @@ from .models import StockInstance, StockFood, Food
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.db.models import Q
+from django.http import Http404
 
 class StockInstanceListView(LoginRequiredMixin, generic.ListView):
     """class-based list view for StockInstances"""
@@ -42,26 +43,26 @@ class StockFoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a stockfood instance using forms"""
     model=StockFood 
     fields=['stock_instance','food','quantity','expiry_date']
-
-    def get_initial(self):
-        """Set initial field values in form"""
-        initial = super().get_initial()
-        stock_instance_id = self.kwargs.get('pk')
-        if stock_instance_id:
-            initial['stock_instance'] = stock_instance_id
-        return initial
-
-    def get_form(self, form_class=None):
-        """Customize the form to disable the stock_instance field (optional)"""
-        form = super().get_form(form_class)
-        if 'stock_instance' in form.fields:
-            form.fields['stock_instance'].widget.attrs['readonly'] = True  # Make it readonly
-        return form
+    #
+    # def get_initial(self):
+    #     """Set initial field values in form"""
+    #     initial = super().get_initial()
+    #     stock_instance_id = self.kwargs.get('pk')
+    #     if stock_instance_id:
+    #         initial['stock_instance'] = stock_instance_id
+    #     return initial
+    #
+    # def get_form(self,form_class=None):
+    #     """Overiding get_form to modify the forms fields"""
+    #     form = super().get_form(form_class)
+    #     # form.fields['stock_instance'].initial = self.kwargs.get('pk')
+    #     form.fields['stock_instance'].disabled = True
 
 class StockFoodUpdate(LoginRequiredMixin, UpdateView):
-    """generic-edit view to update a stockood instance using forms"""
+    """generic-edit view to update a StockFood instance using forms"""
     model=StockFood 
     fields=['stock_instance','food','quantity','expiry_date']
+
 
 class StockFoodDelete(LoginRequiredMixin, DeleteView):
     """generic-edit view to delete a StockFood using forms"""
@@ -86,13 +87,40 @@ class FoodListView(generic.ListView):
         ).distinct()
             return object_list
         return Food.objects.all()
+    
 
 class FoodDetailView(generic.DetailView):
     """class-based detail view for specific Food instance"""
     model=Food
 
+
 class FoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a food instance using forms"""
     model=Food
-    fields = ['name','category','shelf_life']
+    fields = ['user','name','category','shelf_life']
 
+    def get_form(self, *args, **kwargs):
+        """Overiding get_form to modify the forms fields"""
+        form = super().get_form(*args,**kwargs)
+        form.fields['user'].initial = self.request.user # set user to the logged in user instance
+        form.fields['user'].disabled = True     # Prevent user from selecting other user
+        return form
+
+
+class FoodUpdate(LoginRequiredMixin, generic.UpdateView):
+    """generic-edit view to update a Food instance using forms"""
+    model=Food
+    fields = ['user','name','category','shelf_life']
+
+    def get_object(self, queryset=None):
+        """Overide get_object to verify loggedin user created Food"""
+        food_obj = super().get_object(queryset)
+        if food_obj.user != self.request.user:
+            raise Http404('You are not authorized to update this food')
+        return food_obj
+
+    def get_form(self, *args, **kwargs):
+        """Overiding get_form to modify the forms fields"""
+        form = super().get_form(*args,**kwargs)
+        form.fields['user'].disabled = True     
+        return form
