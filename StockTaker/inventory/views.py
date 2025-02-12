08be742edfbcb116ce083a73899c0bf619d6950f
Naoise Baoise <naoisebaoise@Naoises-MacBook-Air.local>
2025-02-12 17:41:58 +0000

    fix(views-security)-only creater of objects can view,update and delete  model instance pages

--- StockTaker/inventory/views.py
+++ StockTaker/inventory/views.py
@@ -5,7 +5,8 @@ from .models import StockInstance, StockFood, Food
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.db.models import Q
-from django.http import Http404
+from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
 
 class StockInstanceListView(LoginRequiredMixin, generic.ListView):
     """class-based list view for StockInstances"""
@@ -14,54 +15,92 @@ class StockInstanceListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         """Overiding the query for StockInstanceListView"""
         return StockInstance.objects.filter(user=self.request.user)
+    
 
-class StockInstanceDetailView(generic.DetailView):
+class StockInstanceDetailView(LoginRequiredMixin, generic.DetailView):
     """class-based detail view for specific StockInstance"""
     model = StockInstance
+
+    def get_object(self,queryset=None):
+        """Overide get_object to verify logged in user created stockInstance"""
+        stockinstance_obj  = super().get_object(queryset)
+        if stockinstance_obj.user != self.request.user:
+            raise PermissionDenied("You are not authorized to access this inventory")
+        return stockinstance_obj
 
 class StockInstanceCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a StockInstance using forms"""
     model=StockInstance
     fields = ['user','name']
 
+    def get_form(self,form_class=None):
+        """Overiding get_form to initialise form valuer"""
+        form = super().get_form(form_class)
+        form.fields['user'].initial = self.request.user
+        form.fields['user'].disabled = True
+        return form
+
 class StockInstanceUpdate(LoginRequiredMixin, UpdateView):
     """generic-edit view to update a StockInstance using forms"""
     model=StockInstance
     fields = ['user','name']
+
+    def get_object(self,queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockInstance_obj = super().get_object(queryset)
+        if StockInstance_obj.user != self.request.user:
+            raise PermissionDenied("You are not authorized to delete this StockInstance")
+        return stockFood_obj
 
 class StockInstanceDelete(LoginRequiredMixin, DeleteView):
     """generic-edit view to delete a StockInstance using forms"""
     model=StockInstance
     success_url = reverse_lazy('homepage')   
 
+    def get_object(self,queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockInstance_obj = super().get_object(queryset)
+        if StockInstance_obj.user != self.request.user:
+            raise PermissionDenied("You are not authorized to delete this StockInstance")
+        return StockInstance_obj
+
+
 
 class StockFoodDetail(LoginRequiredMixin, generic.DetailView):
     """class-based detail view to display specific StockFood instance"""
     model=StockFood
 
+    def get_object(self,queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockFood_obj = super().get_object(queryset)
+        if StockFood_obj.stock_instance.user != self.request.user:
+            raise PermissionDenied("You are not authorized to access this stockfood")
+        return StockFood_obj
+
 class StockFoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a stockfood instance using forms"""
     model=StockFood 
     fields=['stock_instance','food','quantity','expiry_date']
-    #
-    # def get_initial(self):
-    #     """Set initial field values in form"""
-    #     initial = super().get_initial()
-    #     stock_instance_id = self.kwargs.get('pk')
-    #     if stock_instance_id:
-    #         initial['stock_instance'] = stock_instance_id
-    #     return initial
-    #
-    # def get_form(self,form_class=None):
-    #     """Overiding get_form to modify the forms fields"""
-    #     form = super().get_form(form_class)
-    #     # form.fields['stock_instance'].initial = self.kwargs.get('pk')
-    #     form.fields['stock_instance'].disabled = True
+
+    def get_form(self,form_class=None):
+        """Overiding get_form to modify the forms fields"""
+        form = super().get_form(form_class)
+        
+        form.fields['stock_instance'].initial = self.kwargs.get('pk')
+        form.fields['stock_instance'].disabled = True
+        return form
 
 class StockFoodUpdate(LoginRequiredMixin, UpdateView):
     """generic-edit view to update a StockFood instance using forms"""
     model=StockFood 
     fields=['stock_instance','food','quantity','expiry_date']
+
+    def get_object(self,queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockFood_obj = super().get_object(queryset)
+        if StockFood_obj.stock_instance.user != self.request.user:
+            raise PermissionDenied("You are not authorized to change this stockfood")
+        return StockFood_obj
 
 
 class StockFoodDelete(LoginRequiredMixin, DeleteView):
@@ -72,6 +111,14 @@ class StockFoodDelete(LoginRequiredMixin, DeleteView):
     def get_success_url(self):
         stockinstance = self.object.stock_instance 
         return reverse('stockInstance-detail', kwargs={'pk':stockinstance.id})
+
+    def get_object(self,queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockFood_obj = super().get_object(queryset)
+        if StockFood_obj.stock_instance.user != self.request.user:
+            raise PermissionDenied("You are not authorized to delete this stockfood")
+        return StockFood_obj
+
 
 class FoodListView(generic.ListView):
     """class-based list view for Food objects"""
@@ -94,14 +141,15 @@ class FoodDetailView(generic.DetailView):
     model=Food
 
 
+
 class FoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a food instance using forms"""
     model=Food
     fields = ['user','name','category','shelf_life']
 
-    def get_form(self, *args, **kwargs):
+    def get_form(self,form_class=None):
         """Overiding get_form to modify the forms fields"""
-        form = super().get_form(*args,**kwargs)
+        form = super().get_form(form_class)
         form.fields['user'].initial = self.request.user # set user to the logged in user instance
         form.fields['user'].disabled = True     # Prevent user from selecting other user
         return form
@@ -113,14 +161,29 @@ class FoodUpdate(LoginRequiredMixin, generic.UpdateView):
     fields = ['user','name','category','shelf_life']
 
     def get_object(self, queryset=None):
-        """Overide get_object to verify loggedin user created Food"""
+        """Overide get_object to verify logged in user created Food"""
         food_obj = super().get_object(queryset)
         if food_obj.user != self.request.user:
-            raise Http404('You are not authorized to update this food')
+            raise PermissionDenied('You are not authorized to update this food')
         return food_obj
 
-    def get_form(self, *args, **kwargs):
+    def get_form(self,form_class=None):
         """Overiding get_form to modify the forms fields"""
-        form = super().get_form(*args,**kwargs)
+        form = super().get_form(form_class)
         form.fields['user'].disabled = True     
         return form
+
+class FoodDelete(LoginRequiredMixin,generic.DeleteView):
+    """generic-edit view to delete a Food instance using forms"""
+    model=Food
+    success_url = reverse_lazy('all-foods')
+
+    def get_object(self, queryset=None):
+        """Overide get_object to verify logged in user created Food"""
+        food_obj = super().get_object(queryset)
+        if food_obj.user != self.request.user:
+            raise PermissionDenied('You are not authorized to delete this food')
+        return food_obj
+
+
+
