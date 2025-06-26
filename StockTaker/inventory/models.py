7d2479948bf0b6627a2d54c251094b88f0eedde5
Naoise Baoise <naoisebaoise@Naoises-MacBook-Air.local>
2025-06-26 20:00:09 +0100

    update(filtering and templates)

--- StockTaker/inventory/models.py
+++ StockTaker/inventory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import CharField,ManyToManyField,DurationField,ForeignKey,BigIntegerField,PositiveSmallIntegerField,DateTimeField,TextField,BooleanField
+from django.db.models import CharField, ManyToManyField, DurationField, ForeignKey, BigIntegerField, PositiveSmallIntegerField, DateTimeField, TextField, BooleanField
 from .fields import TimeFrameField, pluralize, string_to_days
 from django.db.models import UniqueConstraint
 from django.core.exceptions import ValidationError
@@ -7,60 +7,70 @@ from datetime import timedelta
 from django.db.models.functions import Lower
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Food(models.Model):
     """Model representing a specific food item"""
-    user = ForeignKey(User,on_delete=models.SET_NULL,null=True,db_index=True)
+    user = ForeignKey(User, on_delete=models.SET_NULL,
+                      null=True, db_index=True)
     name = CharField(max_length=200)
-    category = ManyToManyField('Category',help_text="Enter the foods category e.g(milk)")
-    shelf_life = TimeFrameField(help_text='Enter the number and timeframe (days/weeks/months/years), e.g. 4 weeks')
+    category = ManyToManyField(
+        'Category', help_text="Enter the foods category e.g(milk)")
+    shelf_life = TimeFrameField(
+        help_text='Shelf life is optional , e.g. 4 weeks')
     verified = BooleanField(null=True)
-    
+
     def __str__(self):
         """String representation of a food object"""
         return self.name
-           
+
     def get_absolute_url(self):
         """returns url for particular Food instance"""
-        return reverse('food-detail',args=[str(self.id)])
+        return reverse('food-detail', args=[str(self.id)])
 
     def display_category(self):
-        """creating a string for category as ManyToManyField cost in admin model list_display"""
-        return ', '.join(category.category_type for category in self.category.all() )
-    display_category.short_description = 'Categories' # default description for category fields in admin
-
+        """displays all ManyToManyField category in a string"""
+        return ', '.join(category.category_type for category in self.category.all())
+    # default description for category fields in admin
+    display_category.short_description = 'Categories'
 
 
 class Category(models.Model):
     """Model representing food categories"""
-    category_type = CharField(max_length=200,unique=True,help_text='Enter the category type e.g (keto,vegan)',db_index=True)
-    description = TextField(help_text='Enter summary on category type: e.g(vegan: health benefits, environmental impact)')
+    category_type = CharField(max_length=200, unique=True,
+                              help_text='Enter the category type e.g (keto,vegan)', db_index=True)
+    description = TextField(
+        help_text='Enter summary on category type: e.g(vegan: health benefits, environmental impact)')
 
     def __str__(self):
         """String representation of a Category object"""
         return self.category_type
-    
+
     def get_absolute_url(self):
         """returns the url to access a particular Category instance"""
-        return reverse('category-detail', args= [str(self.id)] )
+        return reverse('category-detail', args=[str(self.id)])
 
     class Meta:
-        constraints= [
-        UniqueConstraint(
+        constraints = [
+            UniqueConstraint(
                 Lower('category_type'),
-                name = 'Category_category_type_case_insensitive_unique',
+                name='Category_category_type_case_insensitive_unique',
                 violation_error_message="Category already exsists (case insensitive match)"
-                      ),
+            ),
         ]
-       
+
+
 class StockFood(models.Model):
     """Model representing a StockInstance Food object"""
-    stock_instance = ForeignKey('StockInstance',on_delete=models.CASCADE,db_index=True)
-    food = ForeignKey(Food,on_delete=models.CASCADE)
+    stock_instance = ForeignKey(
+        'StockInstance', on_delete=models.CASCADE, db_index=True)
+    food = ForeignKey(Food, on_delete=models.CASCADE)
     quantity = PositiveSmallIntegerField(default=1)
-    added_date = DateTimeField(auto_now_add=True) #remember to exclude in forms
-    expiry_date = DateTimeField(help_text='Enter the expiry date (optional)',blank=True,null=True)
+    # remember to exclude in forms
+    added_date = DateTimeField(auto_now_add=True)
+    expiry_date = DateTimeField(
+        help_text='Enter the expiry date (optional)', blank=True, null=True)
 
     def __str__(self):
         """String representation of a stockFood"""
@@ -68,75 +78,86 @@ class StockFood(models.Model):
 
     def get_absolute_url(self):
         """returns the url to access a particular StockFood instance"""
-        return reverse('stockFood-detail', args=[ str(self.id)] )
+        return reverse('stockFood-detail', args=[str(self.id)])
 
     def save(self, *args, **kwargs):
-        """Save method overided to calculate expiry_date"""
-        super().save(*args,**kwargs)
+        """Save method overided to compute fields"""
+        super().save(*args, **kwargs)
         if self.expiry_date is None:
             shelf_life_days = string_to_days(self.food.shelf_life)
             shelf_life_timedelta = timedelta(days=shelf_life_days)
             self.expiry_date = self.added_date + shelf_life_timedelta
-            super().save(update_fields=['expiry_date']) # optimised query to just save()
+            # optimised query to just save()
+            super().save(update_fields=['expiry_date'])
+        # update food verification status
         self.update_food_verification(self.food)
-       
-    def delete(self,*args,**kwargs):
+
+    @property
+    def expired(self):
+        """determines if food has expired"""
+        return self.expiry_date < timezone.now()
+
+    def delete(self, *args, **kwargs):
         """Overiding delete method to update food field"""
         food_instance = self.food
-        super().delete(*args,**kwargs)
+        super().delete(*args, **kwargs)
         self.update_food_verification(food_instance)
 
     @staticmethod
     def update_food_verification(food_instance):
         """method to update the verification status of a food instance specified in StockFood instance"""
         count = StockFood.objects.filter(food=food_instance).count()
-        if count >= 10 and not food_instance.verified: 
+        if count >= 10 and not food_instance.verified:
             food_instance.verified = True
             food_instance.save(update_fields=['verified'])
 
-        elif count<10 and food_instance.verified:
+        elif count < 10 and food_instance.verified:
             food_instance.verified = False
             food_instance.save(update_fields=['verified'])
 
+
 class StockInstance(models.Model):
     """Model representing a specific users stock"""
-    user = ForeignKey(User,on_delete=models.CASCADE,db_index=True)
-    name = CharField(max_length=200,default= 'stockroom')
-    
+    user = ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    name = CharField(max_length=200, default='stockroom')
+
     def __str__(self):
         """String representation of a stockInstance object"""
         return self.name
 
     def get_absolute_url(self):
         """returns the url to access a particular stockInstance object"""
-        return reverse('stockInstance-detail', args=[str(self.id)] )
+        return reverse('stockInstance-detail', args=[str(self.id)])
 
     @property
     def num_items(self):
         """returns the number of StockFood objects associated with stockInstance"""
         num = 0
-        query = stockFood.objects.all().filter(stock_instance_id=self.id)
+        query = StockFood.objects.all().filter(stock_instance_id=self.id)
         if query:
             for food in query:
                 num += food.quantity
         return num
-        
+
     def save(self, *args, **kwargs):
         """Overiding save method to produce a customised default StockInstance name"""
         super().save(*args, **kwargs)
         if self.name == 'stockroom':
             self.name = self.user.username + " stockroom"
             super().save(update_fields=['name'])
-   
+
+
 class Recipe(models.Model):
     """Model representing a Recipe object"""
     user = ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     name = CharField(max_length=200)
-    ingredients = ManyToManyField(Food, help_text='Enter the recipes ingredients')
-    category = ManyToManyField(Category, help_text='Enter the dietary category e.g Vegan, Gluten Free') 
+    ingredients = ManyToManyField(
+        Food, help_text='Enter the recipes ingredients')
+    category = ManyToManyField(
+        Category, help_text='Enter the dietary category e.g Vegan, Gluten Free')
     serving_sizes = (
-        ('g','grams'),
-        ('kg','kilograms'),
+        ('g', 'grams'),
+        ('kg', 'kilograms'),
         ('oz', 'ounces'),
         ('ml', 'milliliters')
     )
@@ -146,15 +167,22 @@ class Recipe(models.Model):
                              default='g',
                              help_text='Enter the measurement for portion of recipe')
     portion_quantity = PositiveSmallIntegerField(default=250)
-    instructions = TextField(help_text='Enter a clear concise method for making this recipe.')
+    instructions = TextField(
+        help_text='Enter a clear concise method for making this recipe.')
     created_at = DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """String representation of a Recipe object"""
         return self.name
-    
+
     def get_absolute_url(self):
         """returns a url to access a particular Recipe instance"""
-        return reverse('recipe-detail', args =[str(self.id)] )
+        return reverse('recipe-detail', args=[str(self.id)])
 
+    def display_ingredients(self):
+        """displays ManyToManyField ingredients in one continious string"""
+        return ', '.join(food.name for food in self.ingredients.all())
 
+    def display_category(self):
+        """displays ManyToManyField category in one continious string"""
+        return ', '.join(category.category_type for category in self.category.all())
--- StockTaker/inventory/views.py
+++ StockTaker/inventory/views.py
@@ -1,132 +1,214 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import StockInstance, StockFood, Food
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from .models import StockInstance, StockFood, Food, Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.db.models import Q
+from django.db.models import Prefetch
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
+from django.core.paginator import Paginator
+from django.utils import timezone
 
 
 class StockInstanceListView(LoginRequiredMixin, generic.ListView):
     """class-based list view for StockInstances"""
     model = StockInstance
-    
+
     def get_queryset(self):
         """Overiding the query for StockInstanceListView"""
         return StockInstance.objects.filter(user=self.request.user)
-    
+
 
 class StockInstanceDetailView(LoginRequiredMixin, generic.DetailView):
     """class-based detail view for specific StockInstance"""
     model = StockInstance
 
-    def get_object(self,queryset=None):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        foods = StockFood.objects.filter(
+            stock_instance=self.kwargs['pk']).distinct()
+
+        # filtering foods
+        food_type = self.request.GET.get('q')
+        filter = self.request.GET.get('filter')
+        if food_type:
+            id = Q(stock_instance=self.kwargs['pk'])
+            name_category = Q(food__name__icontains=food_type) | Q(
+                food__category__category_type__icontains=food_type)
+            filtered_foods = StockFood.objects.filter(
+                Q(id & name_category)
+            ).distinct()
+            foods = filtered_foods
+        if filter:
+            foods = foods.filter(expiry_date__lt=timezone.now())
+        # paginate
+        paginator = Paginator(foods, 6)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context["page_obj"] = page_obj
+        return context
+
+    def get_object(self, queryset=None):
         """Overide get_object to verify logged in user created stockInstance"""
-        stockinstance_obj  = super().get_object(queryset)
+        stockinstance_obj = super().get_object(queryset)
         if stockinstance_obj.user != self.request.user:
-            raise PermissionDenied("You are not authorized to access this inventory")
+            raise PermissionDenied(
+                "You are not authorized to access this inventory")
         return stockinstance_obj
+
 
 class StockInstanceCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a StockInstance using forms"""
-    model=StockInstance
-    fields = ['user','name']
+    model = StockInstance
+    fields = ['user', 'name']
 
-    def get_form(self,form_class=None):
+    def get_form(self, form_class=None):
         """Overiding get_form to initialise form valuer"""
         form = super().get_form(form_class)
         form.fields['user'].initial = self.request.user
         form.fields['user'].disabled = True
         return form
 
+
 class StockInstanceUpdate(LoginRequiredMixin, UpdateView):
     """generic-edit view to update a StockInstance using forms"""
-    model=StockInstance
-    fields = ['user','name']
+    model = StockInstance
+    fields = ['user', 'name']
 
-    def get_object(self,queryset=None):
+    def get_object(self, queryset=None):
         """Overiding get_object method to prevent unauthorized access"""
         StockInstance_obj = super().get_object(queryset)
         if StockInstance_obj.user != self.request.user:
-            raise PermissionDenied("You are not authorized to delete this StockInstance")
-        return stockFood_obj
+            raise PermissionDenied(
+                "You are not authorized to delete this StockInstance")
+        return StockInstance_obj
+
 
 class StockInstanceDelete(LoginRequiredMixin, DeleteView):
     """generic-edit view to delete a StockInstance using forms"""
-    model=StockInstance
-    success_url = reverse_lazy('homepage')   
+    model = StockInstance
+    success_url = reverse_lazy('homepage')
 
-    def get_object(self,queryset=None):
+    def get_object(self, queryset=None):
         """Overiding get_object method to prevent unauthorized access"""
         StockInstance_obj = super().get_object(queryset)
         if StockInstance_obj.user != self.request.user:
-            raise PermissionDenied("You are not authorized to delete this StockInstance")
+            raise PermissionDenied(
+                "You are not authorized to delete this StockInstance")
         return StockInstance_obj
-
 
 
 class StockFoodDetail(LoginRequiredMixin, generic.DetailView):
     """class-based detail view to display specific StockFood instance"""
-    model=StockFood
+    model = StockFood
 
-    def get_object(self,queryset=None):
+    def get_object(self, queryset=None):
         """Overiding get_object method to prevent unauthorized access"""
         StockFood_obj = super().get_object(queryset)
         if StockFood_obj.stock_instance.user != self.request.user:
-            raise PermissionDenied("You are not authorized to access this stockfood")
+            raise PermissionDenied(
+                "You are not authorized to access this stockfood")
         return StockFood_obj
+
 
 class StockFoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a stockfood instance using forms"""
-    model=StockFood 
-    fields=['stock_instance','food','quantity','expiry_date']
+    model = StockFood
+    fields = ['food', 'quantity', 'expiry_date']
 
-    def get_form(self,form_class=None):
-        """Overiding get_form to modify the forms fields"""
-        form = super().get_form(form_class)
-        
-        form.fields['stock_instance'].initial = self.kwargs.get('pk')
-        form.fields['stock_instance'].disabled = True
-        return form
-
-class StockFoodUpdate(LoginRequiredMixin, UpdateView):
-    """generic-edit view to update a StockFood instance using forms"""
-    model=StockFood 
-    fields=['stock_instance','food','quantity','expiry_date']
-
-    def get_object(self,queryset=None):
+    def get_object(self, queryset=None):
         """Overiding get_object method to prevent unauthorized access"""
         StockFood_obj = super().get_object(queryset)
         if StockFood_obj.stock_instance.user != self.request.user:
-            raise PermissionDenied("You are not authorized to change this stockfood")
+            raise PermissionDenied(
+                "You are not authorized to change this stockfood")
         return StockFood_obj
+
+    def get_form(self, form_class=None):
+        """Overiding get_form to modify the forms fields"""
+        form = super().get_form(form_class)
+        request_stock_instance = StockInstance.objects.get(
+            id=self.kwargs['pk'])
+        print(request_stock_instance)
+        form.instance.stock_instance = request_stock_instance
+        return form
+
+
+# class StockFoodForm(forms.ModelForm):
+#     class Meta:
+#         model = StockFood
+#         fields = ['stock_instance', 'food', 'quantity', 'expiry_date']
+#         widgets = {
+#             'food': forms.TextInput(attrs={'disabled': 'disabled'}),
+#             'stock_instance': forms.TextInput(attrs={'disabled': 'disabled'}),
+#         }
+#         labels = {
+#             'food': 'Food',  # optional: no label
+#             'stock_instance': 'Stock Room',
+#         }
+#         help_texts = {
+#             'food': '',
+#             'stock_instance': '',
+#             'expiry_date': '',
+#         }
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         if self.instance:
+#             self.fields['food'].initial = str(self.instance.food)
+#             self.fields['stock_instance'].initial = str(
+#                 self.instance.stock_instance)
+#
+
+class StockFoodUpdate(LoginRequiredMixin, UpdateView):
+    """generic-edit view to update a StockFood instance using forms"""
+    model = StockFood
+    # form_class = StockFoodForm
+    fields = ['stock_instance', 'food', 'quantity', 'expiry_date']
+
+    def get_object(self, queryset=None):
+        """Overiding get_object method to prevent unauthorized access"""
+        StockFood_obj = super().get_object(queryset)
+        if StockFood_obj.stock_instance.user != self.request.user:
+            raise PermissionDenied(
+                "You are not authorized to change this stockfood")
+        return StockFood_obj
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['stock_instance'].widget = forms.HiddenInput()
+        form.fields['food'].widget = forms.HiddenInput()
+        form.fields['expiry_date'].help_text = ""
+        return form
 
 
 class StockFoodDelete(LoginRequiredMixin, DeleteView):
     """generic-edit view to delete a StockFood using forms"""
-    model=StockFood
+    model = StockFood
     success_url = reverse_lazy('')
 
     def get_success_url(self):
-        stockinstance = self.object.stock_instance 
-        return reverse('stockInstance-detail', kwargs={'pk':stockinstance.id})
+        stockinstance = self.object.stock_instance
+        return reverse('stockInstance-detail', kwargs={'pk': stockinstance.id})
 
-    def get_object(self,queryset=None):
+    def get_object(self, queryset=None):
         """Overiding get_object method to prevent unauthorized access"""
         StockFood_obj = super().get_object(queryset)
         if StockFood_obj.stock_instance.user != self.request.user:
-            raise PermissionDenied("You are not authorized to delete this stockfood")
+            raise PermissionDenied(
+                "You are not authorized to delete this stockfood")
         return StockFood_obj
-    
-
 
 
 class FoodListView(generic.ListView):
     """class-based list view for Food objects"""
     model = Food
-    
+    paginate_by = 6
+
     def get_queryset(self):
         """Overiding queryset for search result"""
         query = self.request.GET.get('q')
@@ -134,60 +216,151 @@ class FoodListView(generic.ListView):
             object_list = Food.objects.filter(
                 Q(name__icontains=query) |
                 Q(category__category_type__icontains=query)
-        ).distinct()
+            ).distinct()
             return object_list
         return Food.objects.all()
-    
+
 
 class FoodDetailView(generic.DetailView):
     """class-based detail view for specific Food instance"""
-    model=Food
-
+    model = Food
 
 
 class FoodCreate(LoginRequiredMixin, CreateView):
     """generic-edit view to create a food instance using forms"""
-    model=Food
-    fields = ['user','name','category','shelf_life']
+    model = Food
+    fields = ['name', 'category', 'shelf_life']
 
-    def get_form(self,form_class=None):
+    def get_form(self, form_class=None):
         """Overiding get_form to modify the forms fields"""
         form = super().get_form(form_class)
-        form.fields['user'].initial = self.request.user # set user to the logged in user instance
-        form.fields['user'].disabled = True     # Prevent user from selecting other user
+        # set user to the logged in user instance
+        # form.fields['user'].initial = self.request.user
+        # Prevent user from selecting other user
+        # form.fields['user'].disabled = True
+        # form.fields['user'].widget = forms.HiddenInput()
+        form.fields['category'].help_text = None
+        # form.fields['shelf_life'].help_text = None
         return form
 
 
 class FoodUpdate(LoginRequiredMixin, generic.UpdateView):
     """generic-edit view to update a Food instance using forms"""
-    model=Food
-    fields = ['user','name','category','shelf_life']
+    model = Food
+    fields = ['user', 'name', 'category', 'shelf_life']
 
     def get_object(self, queryset=None):
         """Overide get_object to verify logged in user created Food"""
         food_obj = super().get_object(queryset)
         if food_obj.user != self.request.user:
-            raise PermissionDenied('You are not authorized to update this food')
+            raise PermissionDenied(
+                'You are not authorized to update this food')
         return food_obj
 
-    def get_form(self,form_class=None):
+    def get_form(self, form_class=None):
         """Overiding get_form to modify the forms fields"""
         form = super().get_form(form_class)
-        form.fields['user'].disabled = True     
+        form.fields['user'].disabled = True
         return form
 
-class FoodDelete(LoginRequiredMixin,generic.DeleteView):
+
+class FoodDelete(LoginRequiredMixin, generic.DeleteView):
     """generic-edit view to delete a Food instance using forms"""
-    model=Food
+    model = Food
     success_url = reverse_lazy('all-foods')
 
     def get_object(self, queryset=None):
         """Overide get_object to verify logged in user created Food"""
         food_obj = super().get_object(queryset)
         if food_obj.user != self.request.user:
-            raise PermissionDenied('You are not authorized to delete this food')
+            raise PermissionDenied(
+                'You are not authorized to delete this food')
         return food_obj
-    
 
 
+class RecipeListView(generic.ListView):
+    """class-based List view for recpipes"""
+    model = Recipe
+    paginate_by = 6
 
+    def get_queryset(self):
+        recipe_filter = self.request.GET.get("q")
+        if recipe_filter:
+            filter_query = Q(name__icontains=recipe_filter) | Q(
+                category__category_type__icontains=recipe_filter)
+            recipe_filter = Recipe.objects.filter(filter_query).distinct()
+            print(recipe_filter)
+            return recipe_filter
+        return Recipe.objects.all()
+
+
+class RecipeDetailView(generic.DetailView):
+    """class-based detail view for specific Recipe isntance"""
+    model = Recipe
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        recipe = self.get_object()
+        context['instruction_list'] = recipe.instructions.splitlines()
+
+        return context
+
+
+class RecipeCreate(LoginRequiredMixin, generic.CreateView):
+    """generic-edit view to create a Recipe instance using forms"""
+    model = Recipe
+    fields = ['user', 'name', 'ingredients', 'category',
+              'portion_size', 'portion_quantity', 'instructions']
+
+    def get_form(self, form_class=None):
+        """Overiding get_form to modify form fields"""
+        form = super().get_form(form_class)
+        form.fields['user'].initial = self.request.user
+        form.fields['user'].widget = forms.HiddenInput()
+        form.fields['category'].help_text = None
+        form.fields['portion_size'].help_text = None
+        form.fields['ingredients'].help_text = None
+        form.fields['instructions'].help_text = None
+
+        return form
+
+
+class RecipeUpdate(LoginRequiredMixin, generic.UpdateView):
+    """generic-edit view to update a recipe instance using forms"""
+    model = Recipe
+    fields = ['user', 'name', 'ingredients', 'category',
+              'portion_size', 'portion_quantity', 'instructions']
+
+    def get_object(self, queryset=None):
+        """overide get_object to verify logged in user created Recipe"""
+        recipe_obj = super().get_object(queryset)
+        if recipe_obj.user != self.request.user:
+            raise PermissionDenied(
+                "You are not authorized to update this recipe")
+        return recipe_obj
+
+    def get_form(self, form_class=None):
+        """Overiding get_form to modify form fields"""
+        form = super().get_form(form_class)
+        form.fields['user'].initial = self.request.user
+        form.fields['user'].widget = forms.HiddenInput()
+        form.fields['category'].help_text = None
+        form.fields['portion_size'].help_text = None
+        form.fields['ingredients'].help_text = None
+        form.fields['instructions'].help_text = None
+
+        return form
+
+
+class RecipeDelete(LoginRequiredMixin, generic.DeleteView):
+    """generic-edit view to delete a recipe instance using forms"""
+    model = Recipe
+    success_url = reverse_lazy('all-recipes')
+
+    def get_object(self, queryset=None):
+        """override get_object to verify logged in user created Recipe"""
+        recipe_obj = super().get_object(queryset)
+        if recipe_obj.user != self.request.user:
+            raise PermissionDenied(
+                "You are not authorized to delete this recipe")
+        return recipe_obj
